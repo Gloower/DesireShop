bc21f9a4985892d2954ff9fd5dc4bec4068f5c1c
Gloower <gloowerzin@gmail.com>
2023-03-09 15:19:56 -0300

    First commit of the new project

---
+++ src/test/java/br/com/desireshop/shopping/ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desireshop.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
